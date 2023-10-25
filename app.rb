c83c1743e516bbab75afa16ac504de66a2166ded
Mary Webby <118230483+marywebby@users.noreply.github.com>
2023-10-25 23:35:35 +0000

    final commit

--- app.rb
+++ app.rb
@@ -5,43 +5,17 @@ get("/") do
   erb(:homepage)
 end
 
-get("/dice/2/6") do
-  first_die = rand(1..6)
-	second_die = rand(1..6)
-  sum = first_die + second_die
-	
-	@outcome = "You rolled a #{first_die} and a #{second_die} for a total of #{sum}."
-	
-	erb(:two_six)
-end
+get("/dice/:number_of_rolls/:sides") do 
+  @num_dice = params.fetch("number_of_rolls").to_i
+  @num_sides= params.fetch("sides").to_i
 
-get("/dice/2/10") do
-  first_die = rand(1..10)
-	second_die = rand(1..10)
-  sum = first_die + second_die
-	
-	@outcome = "You rolled a #{first_die} and a #{second_die} for a total of #{sum}."
-	
-  erb(:two_ten)
-end
+  @rolls = []
 
-get("/dice/1/20") do
-  @die = rand(1..20)
-	
-	@outcome = "You rolled a #{@die}."
-	
-	erb(:one_twenty)
-end
+  @num_dice.times do
+    die = rand(1..@num_sides)
 
-get("/dice/5/4") do
-  first_die = rand(1..4)
-	second_die = rand(1..4)
-  third_die = rand(1..4)
-  fourth_die = rand(1..4)
-  fifth_die = rand(1..4)
-  sum = first_die + second_die + third_die + fourth_die + fifth_die
-	
-	@outcome = "You rolled a #{first_die}, #{second_die}, #{third_die}, #{fourth_die}, and #{fifth_die} for a total of #{sum}."
-	
-  erb(:five_four)
+    @rolls.push(die)
+  end 
+
+  erb(:flexible)
 end
